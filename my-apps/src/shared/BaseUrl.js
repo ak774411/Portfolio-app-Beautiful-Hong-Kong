4ff61d7ae12190199cace6f9645c5d45881c36fc
Michael Cheng <yuenming54@gmail.com>
2021-08-10 01:15:40 +0800

    sasassasaas

--- my-apps/src/shared/BaseUrl.js
+++ my-apps/src/shared/BaseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:3001/';
+export const baseUrl = 'http://www.chengyuenming.com/';
 
--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:3001'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:3001','http://www.chengyuenming.com','https://www.chengyuenming.com','https://morning-shore-13345.herokuapp.com/'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
