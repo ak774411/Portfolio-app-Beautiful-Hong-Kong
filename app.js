35554a655a92c6b22a95142b3d30949953379ad1
Michael Cheng <yuenming54@gmail.com>
2021-08-10 00:33:35 +0800

    sssss

--- app.js
+++ app.js
@@ -63,13 +63,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 //app.use(passport.session());
 __dirname = path.resolve();
-if(process.env.NODE_ENV=='production'){
-  app.use(express.static(path.join(__dirname,'/client/build')));
+app.use(express.static(path.join(__dirname,'/client/build')));
 
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-  })
-}
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+})
+
 app.use('/users', usersRouter);
 /*function auth (req, res, next) {
 
