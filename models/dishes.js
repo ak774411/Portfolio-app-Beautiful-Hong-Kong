e64ef3b23a278f6305bc1311cff38f69412010c5
Michael Cheng <yuenming54@gmail.com>
2021-08-15 17:53:05 +0800

    project start

--- models/dishes.js
+++ models/dishes.js
@@ -34,6 +34,22 @@ var dishSchema = new Schema({
     featured:{
         type: Boolean,
         required: false
+    },
+    Distance:{
+        type: String,
+        required: false
+    },
+    Duration:{
+        type: String,
+        required: false
+    },
+    Difficulty:{
+        type: String,
+        required: false
+    },
+    transport:{
+        type: String,
+        required: false
     }
 }, {
     timestamps: true
