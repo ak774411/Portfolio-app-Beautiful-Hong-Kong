f5e703f9276bf62b0579261c0996c3541e000375
Michael Cheng <yuenming54@gmail.com>
2021-08-15 22:02:41 +0800

    sasasa

--- client/src/components/HeaderComponent.js
+++ client/src/components/HeaderComponent.js
@@ -107,12 +107,12 @@ class Header extends Component {
                         </Collapse>
                     </div>
                 </Navbar>
-                <Jumbotron>
+                <Jumbotron style="background-image: url(http://www.chengyuenming.com/images/wallpaper.jpeg);">
                     <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
-                                <h1>Ristorante Con Fusion</h1>
-                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                                <h1>Beautiful hong kong hiking trail</h1>
+                                <p>Hi! I am hiking enthusiast, I am building a website sharing different trail famous in Hong Kong. Let me introduce more hiking trails not discovered yet in Hong Kong!</p>
                             </div>
                         </div>
                     </div>
