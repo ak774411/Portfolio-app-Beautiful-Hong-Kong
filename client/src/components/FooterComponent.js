0b073b66a25109b4ba26ce69c44683b180aae432
Michael Cheng <yuenming54@gmail.com>
2021-08-15 23:00:16 +0800

    SSSSS

--- client/src/components/FooterComponent.js
+++ client/src/components/FooterComponent.js
@@ -9,10 +9,10 @@ function Footer(props) {
                     <div className="col-4 offset-1 col-sm-2">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
-                            <li style={{ color:'white' }}><Link to="/home">Home</Link></li>
-                            <li style={{ color:'white' }}><Link to="/aboutus">About Us</Link></li>
-                            <li style={{ color:'white' }}><Link to="/menu">Menu</Link></li>
-                            <li style={{ color:'white' }}><Link to="/contactus">Contact Us</Link></li>
+                            <li><Link to="/home" style={{ color:'white' }}>Home</Link></li>
+                            <li><Link to="/aboutus" style={{ color:'white' }}>About Us</Link></li>
+                            <li><Link to="/menu" style={{ color:'white' }}>Menu</Link></li>
+                            <li><Link to="/contactus" style={{ color:'white' }}>Contact Us</Link></li>
                         </ul>
                     </div>
                     <div className="col-7 col-sm-5">
